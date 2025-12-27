66d378090dd0da6921750370678e14267eb07bf6
marteagb <abm_8249@outlook.com>
2025-12-26 19:30:20 -0500

    refactor(seeder): ajuste de edades para aulas

--- database/seeders/AulaSeeder.php
+++ database/seeders/AulaSeeder.php
@@ -17,25 +17,32 @@ class AulaSeeder extends Seeder
             'nombre' => 'Primeros pasos',
             'descripcion' => 'Aula para los niños que están dando sus primeros pasos en el aprendizaje.',
             'edad_min' => 2,
-            'edad_max' => 3
+            'edad_max' => 4
         ]);
         Aula::firstOrCreate([
             'nombre' => 'Párvulos',
             'descripcion' => 'Aula para los párvulos que están comenzando su educación inicial.',
-            'edad_min' => 4,
-            'edad_max' => 6
+            'edad_min' => 5,
+            'edad_max' => 7
         ]);
         Aula::firstOrCreate([
             'nombre' => 'Principiantes',
             'descripcion' => 'Aula para los niños que están iniciando su educación básica.',
             'edad_min' => 7,
-            'edad_max' => 10
+            'edad_max' => 9
         ]);
         Aula::firstOrCreate([
             'nombre' => 'Primarios',
             'descripcion' => 'Aula para los estudiantes de educación primaria.',
-            'edad_min' => 11,
-            'edad_max' => 14
+            'edad_min' => 10,
+            'edad_max' => 12
+        ]);
+
+        Aula::firstOrCreate([
+            'nombre' => 'Adolescentes',
+            'descripcion' => 'Aula para los estudiantes de educación secundaria.',
+            'edad_min' => 13,
+            'edad_max' => 15
         ]);
     }
 }
