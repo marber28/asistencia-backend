b46621cfb10d9e2dd3b431c384e7b391fe611bdd
marteagb <abm_8249@outlook.com>
2026-01-03 23:54:29 -0500

    leccion semanal

--- app/Http/Controllers/Api/DashboardController.php
+++ app/Http/Controllers/Api/DashboardController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\AsistenciaAlumno;
 use Illuminate\Http\Request;
-use App\Models\User;
+use App\Models\Leccion;
 use App\Models\Alumno;
 
 class DashboardController extends Controller
@@ -18,7 +18,18 @@ class DashboardController extends Controller
                 now()->startOfWeek(),
                 now()->endOfWeek()
             ])->count(),
+            'leccion' => $this->currentLeccion(),
             'asistencia_mensual' => AsistenciaAlumno::whereMonth('dia', now()->month)->count(),
         ]);
+    }
+
+    private function currentLeccion() {
+        $data = Leccion::whereBetween('fecha', [
+                now()->startOfWeek(),
+                now()->endOfWeek()
+            ])
+            ->get();
+
+        return $data;
     }
 }
